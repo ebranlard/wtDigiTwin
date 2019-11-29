c0d8318e0fcc97246e8d680788a9e4598dbd4872
Emmanuel Branlard <emmanuel.branlard@nrel.gov>
2019-11-29 14:08:56 -0700

    Update of kalman

--- kalman/kalman.py
+++ kalman/kalman.py
@@ -20,7 +20,7 @@ def fEstimateKFTimeStep(u1,y1,z0,A,B,G,J,P0,Q,R):
     
     # Calculate Kalman gain
     # same as Lk from [1] - And their Rtilde_k is G*P1m*G'+R
-    Kk =  np.dot(P1m,G.T).dot( np.linalg.inv(((G.dot(P1m)).dot(G.T) + R)))   # TODO check me
+    Kk =  np.dot(P1m,G.T).dot( np.linalg.inv(((G.dot(P1m)).dot(G.T) + R))) 
     # update estimate with measurement
     z1 = z1m + Kk.dot(y1 - y1hat)
     
@@ -28,85 +28,85 @@ def fEstimateKFTimeStep(u1,y1,z0,A,B,G,J,P0,Q,R):
     return z1,P1,Kk
 
 
-def fKFDiscretize(Ac,Bc,dt,method='exponential'):
-    """ Discretize the continuous states matrices Ac, Bc
+def fKFDiscretize(Xx,Xu,dt,method='exponential'):
+    """ Discretize the continuous states matrices Xx, Xu
     
     "Real" system:
+        zdot = Xx.x + Xu.u + wd
         zdot = Ac.z + Bc.u + wd
           y  = Gc.z + Jc.u + wn
     "Discretized" system:
-        z_{k+1} = Ad.z_k + Bd.u_k + wd
-          y_k   = Gd.z_k + Jd.u_k + wn 
+        z_{k+1} = Xxd.z_k + Xud.u_k + wd
+        z_{k+1} = Ad.z_k  + Bd.u_k + wd
+          y_k   = Gd.z_k  + Jd.u_k + wn 
 
     INPUTS:
         methods: 'exponential', 'eigenvalues', 'forward_euler'
 
     OUTPUTS:
-       Ad,Bd
+       Xxd,Xud, disrete versions of Xx and Xu
     """
     # --- A matrix
     if method=='exponential':
         # Using matrix exponential directly
-        Ad = expm(Ac * dt)
-        if np.linalg.det(Ac) == 0:
-            # TODO TODO missing square of dt for A?
+        Xxd = expm(Xx * dt)
+        if np.linalg.det(Xx) == 0:
             print('[WARN] Matrix A is singular, using forward euler to discretize B matrix\n' % ())
             # Forward euler
-            Bd = dt * Bc
+            Xud = dt * Xu
         else:
-            mA_B = np.linalg.solve(Ac,Bc)
-            Bd = np.dot( (Ac - np.eye(Ac.shape[0])) ,  mA_B)
+            mA_B = np.linalg.solve(Xx,Xu)
+            Xud = np.dot( (Xx - np.eye(Xx.shape[0])) ,  mA_B)
     elif method=='eigenvalues':
         raise NotImplementedError()
         # Using eigenvalues
-        #Q,Lambda = eig(Ac)
-        #Ad = real(Q * expm(Lambda * dt) / Q)
-        #Bd = Bc * dt
+        #Q,Lambda = eig(Xx) # Need my version of eigenvalue, so postponed to keep the library standalone
+        #Xxd = real(Q * expm(Lambda * dt) / Q)
+        #Xud = Xu * dt
     elif method=='forward_euler':
         # Using forward Euler
-        Ad = np.eye(Ac.shape[0]) + Ac * dt # TODO TODO missing square of dt?
-        Bd = Bc * dt
+        Xxd = np.eye(Xx.shape[0]) + Xx * dt 
+        Xud = Xu * dt
     else:
         raise Exception('Unknown discretization method: %s',method)
-    
-    # --- G,J matrices
-    #Gd = Gc
-    #Jd = Jc
-    return Ad,Bd
 
-def fBuildSystem_Linear(M,C,K,Sa,Sv,Sd,Sp=None,Rp=None,Yp=None,Method='default'):
+    return Xxd,Xud
+
+def fBuildSystem_Linear(M,C,K,Ya,Yv,Yq,Fp=None,Pp=None,Yp=None,Method='default'):
     """ Takes system matrices of a mechanical system, returns a state matrix.
-    The state matrix may be an "augmented matrix", in which case Sp, Rp, should be provided
+    The state matrix may be an "augmented matrix", in which case Fp, Pp, should be provided
 
     - Mechanical equation:
-       M qddot + Cqdot + Kq = f
-                                      (f = Sp.p, for augmented)
+       M qddot + Cqdot + Kq = Fp.p + Fu.u 
+
     - Output equation:
-       y = Sa.qddot + Sv.qdot + Sd.q 
-                                      (+ Yp.p, for some augmented system)
+       y = Ya.qddot + Yv.qdot + Yq.q  + Yp.p  + ~Yu.u
+       y = Sa.qddot + Sv.qdot + Sd.q  + Yp.p  + ~Yu.u
     - (Augmented load evolution:
-         pdot = Rp.p , only for augmented system)
+         pdot = Pp.p  + Pq.q + Pv.qdot
 
     State Equation
+        xdot = Xx.x + Xu.u + wd
         zdot = Ac.z + Bc.u + wd
     Measurement Equation
+          y  = Yx.x + Yu.u + wn
           y  = Gc.z + Jc.u + wn
     """
     nDOF = M.shape[0]
-    nY   = Sd.shape[0]
+    nY   = Yq.shape[0]
     if 'default' == Method:
         Z=np.zeros((nDOF,nDOF))
         I=np.eye(nDOF)
-        Ac = np.block( [ [Z , I ], [ mM_K, mM_C] ])
-        Bc = 0
-        Gc = np.block( [ Sd + np.dot(Sa,mM_K),  Sv + np.dot(Sa, mM_C) ] )
-        Jc = 0
+        Xx = np.block( [ [Z , I ], [ mM_K, mM_C] ])
+        Xu = 0
+        Yx = np.block( [ Yq + np.dot(Ya,mM_K),  Yv + np.dot(Ya, mM_C) ] )
+        Yu = 0
     else:
         if 'augmented_first_order' == Method:
-            # Needs Sp and Rp to be defined!
-            if Sp is None or Rp is None:
-                raise Exception('Both Sp and Rp needs to be set with augmented first order method')
-            nP = Sp.shape[1]
+            # Needs Fp and Pp to be defined!
+            if Fp is None or Pp is None:
+                raise Exception('Both Fp and Pp needs to be set with augmented first order method')
+            nP = Fp.shape[1]
             if Yp is None:
                 Yp=np.zeros((nY,nP))
 
@@ -116,52 +116,71 @@ def fBuildSystem_Linear(M,C,K,Sa,Sv,Sd,Sp=None,Rp=None,Yp=None,Method='default')
             I    = np.eye(nDOF)
             mM_K = np.linalg.solve(-M,K)
             mM_C = np.linalg.solve(-M,C)
-            M_Sp  = np.linalg.solve(M,Sp)
-            Ac = np.block( [ [Z, I ,Znnp] , [mM_K, mM_C, M_Sp], [Znpn, Znpn, Rp] ])
-            Bc = 0
-            Gc = np.block( [Sd + np.dot(Sa,mM_K), Sv + np.dot(Sa,mM_C), Yp+np.dot(Sa,M_Sp) ])
-#             print('Sd..:\n', Sd + np.dot(Sa,mM_K))
-#             print('Sv..:\n', Sv + np.dot(Sa,mM_C))
-#             print('Sp..:\n', Yp+np.dot(Sa,M_Sp) )
-            Jc = 0
+            M_Fp  = np.linalg.solve(M,Fp)
+            Xx = np.block( [ [Z, I ,Znnp] , [mM_K, mM_C, M_Fp], [Znpn, Znpn, Pp] ])
+            Xu = 0
+            Yx = np.block( [Yq + np.dot(Ya,mM_K), Yv + np.dot(Ya,mM_C), Yp+np.dot(Ya,M_Fp) ])
+#             print('Yq..:\n', Yq + np.dot(Ya,mM_K))
+#             print('Yv..:\n', Yv + np.dot(Ya,mM_C))
+#             print('Fp..:\n', Yp+np.dot(Ya,M_Fp) )
+            Yu = 0
         else:
             raise Exception('Method %s not implemented')
     
-    return Ac,Bc,Gc,Jc
+    return Xx,Xu,Yx,Yu
 
 
 
 def EmptyStateMat(nX,nU,nY):
     """ Returns state matrices with proper dimensions, filled with 0 """
-    Ac = np.zeros((nX,nX))
-    Gc = np.zeros((nY,nX))
-    Bc = np.zeros((nX,nU))
-    Jc = np.zeros((nY,nU))
-    return Ac,Bc,Gc,Jc
+    Xx = np.zeros((nX,nX)) # Ac 
+    Yx = np.zeros((nY,nX)) # Gc 
+    Xu = np.zeros((nX,nU)) # Xu 
+    Yu = np.zeros((nY,nU)) # Jc 
+    return Xx,Xu,Yx,Yu
 
-def EmptySystemMat(nDOF_2nd, nY, nP=None):
+def EmptySystemMat(nDOF_2nd, nY, nP=None, nU=None):
     """ Returns matrices with proper dimensions, filled with 0
     INPUTS:
        - nDOF_2nd: number of "mechanical" degrees of freedoms, when the equations are written 
                    as a secnod order system, e.g. M qddot = F, then nDOF_2nd is the size of qddot
        - nY: Number of outputs
        - nP: Number of extended loads, if states are to be augmented
+       - nU: Number of inputs
     NOTE:
         full augmented state vector has size 2*nDOF_2nd + nP
     """
     M=np.zeros((nDOF_2nd,nDOF_2nd))
     C=np.zeros((nDOF_2nd,nDOF_2nd))
     K=np.zeros((nDOF_2nd,nDOF_2nd))
-    Sa = np.zeros((nY,nDOF_2nd))
-    Sv = np.zeros((nY,nDOF_2nd))
-    Sd = np.zeros((nY,nDOF_2nd))
+    Ya = np.zeros((nY,nDOF_2nd)) # Sa
+    Yv = np.zeros((nY,nDOF_2nd)) # Sv
+    Yq = np.zeros((nY,nDOF_2nd)) # Sd
+
+    if (nU is not None and nP is None):
+        nP=0
+    elif (nU is None and nP is not None):
+        nU=0
+
+
     if nP is not None:
-        Yp = np.zeros((nY,nP))
-        Sp = np.zeros((nDOF_2nd,nP))
-        Rp = np.zeros((nP,nP))
-        return M,C,K,Sa,Sv,Sd,Sp,Rp,Yp
+        Yp = np.zeros((nY,nP))       # Yp
+        Fp = np.zeros((nDOF_2nd,nP)) # Sp
+        Pp = np.zeros((nP,nP))       # Rp
+        Pv = np.zeros((nDOF_2nd,nP)) 
+        Pq = np.zeros((nDOF_2nd,nP)) 
+
+    if nU is not None:
+        Yu = np.zeros((nY,nU))       
+        Fu = np.zeros((nDOF_2nd,nU)) 
+        Pu = np.zeros((nP,nU))       
+
+
+
+    if (nU is None) and (nP is None):
+        return M,C,K,Ya,Yv,Yq
     else:
-        return M,C,K,Sa,Sv,Sd
+        return M,C,K,Ya,Yv,Yq,Yp,Yu,Fp,Fu,Pp,Pq,Pv
 
 
 
@@ -176,48 +195,48 @@ class Test(unittest.TestCase):
         nX=3
         nU=1
         nY=2
-        Ac, Bc, Gc, Jc = EmptyStateMat(nX,nU,nY)
-        Ac[0,0]=1
-        Ac[0,1]=4
-        Ac[0,2]=4
-        Ac[1,1]=2
-        Ac[1,2]=5
-        Ac[2,2]=3
-        Bc[0,0]=1/2
-        Bc[2,0]=1
+        Xx, Xu, Yx, Yu = EmptyStateMat(nX,nU,nY)
+        Xx[0,0]=1
+        Xx[0,1]=4
+        Xx[0,2]=4
+        Xx[1,1]=2
+        Xx[1,2]=5
+        Xx[2,2]=3
+        Xu[0,0]=1/2
+        Xu[2,0]=1
         dt=0.5
-        Ad,Bd = fKFDiscretize(Ac,Bc,dt,method='exponential')
-        #print('Ac\n',Ad)
-        #print('Bc\n',Bd)
-        Ad_ref=np.array([
+        Xxd,Xud = fKFDiscretize(Xx,Xu,dt,method='exponential')
+        #print('Xx\n',Xxd)
+        #print('Xu\n',Xud)
+        Xxd_ref=np.array([
                [1.64872, 4.27824,12.60440],
                [0.00000, 2.71828, 8.81704],
                [0.00000, 0.00000, 4.48169]])
-        Bd_ref = np.array([ [-2.00000], [ 0.83333], [ 0.66667]])
-        np.testing.assert_almost_equal(Ad,Ad_ref,decimal=5)
-        np.testing.assert_almost_equal(Bd,Bd_ref,decimal=5)
+        Xud_ref = np.array([ [-2.00000], [ 0.83333], [ 0.66667]])
+        np.testing.assert_almost_equal(Xxd,Xxd_ref,decimal=5)
+        np.testing.assert_almost_equal(Xud,Xud_ref,decimal=5)
 
     def test_discretize_forward(self):
         # Discretize a continuous system using the forward euler method
         nX=3
         nU=1
         nY=2
-        Ac, Bc, Gc, Jc = EmptyStateMat(nX,nU,nY)
-        Ac[0,0]=0
-        Ac[0,1]=1
-        Ac[0,2]=0
-        Ac[1,1]=0
-        Ac[1,2]=1
-        Ac[2,2]=0
-        Bc[0,0]=1/2
-        Bc[2,0]=1
+        Xx, Xu, Yx, Yu = EmptyStateMat(nX,nU,nY)
+        Xx[0,0]=0
+        Xx[0,1]=1
+        Xx[0,2]=0
+        Xx[1,1]=0
+        Xx[1,2]=1
+        Xx[2,2]=0
+        Xu[0,0]=1/2
+        Xu[2,0]=1
         dt=0.5
-        Ad_xp,Bd_xp = fKFDiscretize(Ac,Bc,dt,method = 'exponential'  )
-        Ad_fe,Bd_fe = fKFDiscretize(Ac,Bc,dt,method = 'forward_euler')
-        np.testing.assert_almost_equal(Bd_fe,Bd_xp,decimal=5)
+        Xxd_xp,Xud_xp = fKFDiscretize(Xx,Xu,dt,method = 'exponential'  )
+        Xxd_fe,Xud_fe = fKFDiscretize(Xx,Xu,dt,method = 'forward_euler')
+        np.testing.assert_almost_equal(Xud_fe,Xud_xp,decimal=5)
 
         # TODO TODO, what to do of A
-        #np.testing.assert_almost_equal(Ad_fe,Ad_xp,decimal=5)
+        #np.testing.assert_almost_equal(Xxd_fe,Xxd_xp,decimal=5)
 
 
 
@@ -232,28 +251,28 @@ class Test(unittest.TestCase):
         nU       = 0           # Inputs
         J_LSS = 2.0
         # General init
-        M,C,K,Sa,Sv,Sd,Sp,Rp,Yp = EmptySystemMat(nDOF_2nd, nY, nP)
+        M,C,K,Ya,Yv,Yq,Yp,Yu,Fp,Fu,Pp,Pq,Pv = EmptySystemMat (nDOF_2nd, nY, nP, nU)
         # Setting values
         M[0,0]  = J_LSS
-        Sv[0,0] = 1
-        Sp[0,0] = 1
-        Sp[0,1] = -1
+        Yv[0,0] = 1
+        Fp[0,0] = 1
+        Fp[0,1] = -1
         Yp[1,1] = 1 # Direct feed through of force
 
-        Ac,Bc,Gc,Jc = fBuildSystem_Linear(M,C,K,Sa,Sv,Sd,Sp=Sp,Rp=Rp,Yp=Yp,Method='augmented_first_order')
+        Xx,Xu,Yx,Yu = fBuildSystem_Linear(M,C,K,Ya,Yv,Yq,Fp=Fp,Pp=Pp,Yp=Yp,Method='augmented_first_order')
         # Reference values for test
-        Ac_ref, Bc_ref, Gc_ref, Jc_ref = EmptyStateMat(nDOF_ext,nU,nY)
-        Ac_ref[0,1] = 1
-        Ac_ref[1,2] = 1/J_LSS
-        Ac_ref[1,3] = -1/J_LSS
-        Ac_ref[1,3] = -1/J_LSS
-        Gc_ref[0,1] = 1
-        Gc_ref[1,3] = 1
+        Xx_ref, Xu_ref, Yx_ref, Yu_ref = EmptyStateMat(nDOF_ext,nU,nY)
+        Xx_ref[0,1] = 1
+        Xx_ref[1,2] = 1/J_LSS
+        Xx_ref[1,3] = -1/J_LSS
+        Xx_ref[1,3] = -1/J_LSS
+        Yx_ref[0,1] = 1
+        Yx_ref[1,3] = 1
 
-        np.testing.assert_equal(Ac,Ac_ref)
-        np.testing.assert_equal(Bc,Bc_ref)
-        np.testing.assert_equal(Gc,Gc_ref)
-        np.testing.assert_equal(Jc,Jc_ref)
+        np.testing.assert_equal(Xx,Xx_ref)
+        np.testing.assert_equal(Xu,Xu_ref)
+        np.testing.assert_equal(Yx,Yx_ref)
+        np.testing.assert_equal(Yu,Yu_ref)
 
     def test_build_tower1shaft(self):
         # Build continuous matrices for the "Tower (1 mode) + Shaft " case
@@ -268,35 +287,35 @@ class Test(unittest.TestCase):
         C_twr = 10.0
         J_LSS = 2.0
         # General init of zero matrices
-        M,C,K,Sa,Sv,Sd,Sp,Rp,Yp = EmptySystemMat(nDOF_2nd, nY, nP)
+        M,C,K,Ya,Yv,Yq,Yp,Yu,Fp,Fu,Pp,Pq,Pv = EmptySystemMat (nDOF_2nd, nY, nP, nU)
         # Setting values
         M[0,0]  = M_twr
         M[1,1]  = J_LSS
         K[0,0]  = K_twr
         C[0,0]  = C_twr
-        Sa[0,0] = 1  # uddot = qddot[0]
-        Sv[1,1] = 1  # psidot = qdot[1]
+        Ya[0,0] = 1  # uddot = qddot[0]
+        Yv[1,1] = 1  # psidot = qdot[1]
         Yp[2,2] = 1  # Direct feed-through of Mg
-        Sp[0,0] = 1  # T = p[0]
-        Sp[1,1] = 1  # dQ = p[1] -p[2]
-        Sp[1,2] = -1 # dQ = p[1] -p[2]
+        Fp[0,0] = 1  # T = p[0]
+        Fp[1,1] = 1  # dQ = p[1] -p[2]
+        Fp[1,2] = -1 # dQ = p[1] -p[2]
 
-        Ac,Bc,Gc,Jc = fBuildSystem_Linear(M,C,K,Sa,Sv,Sd,Sp=Sp,Rp=Rp,Yp=Yp,Method='augmented_first_order')
+        Xx,Xu,Yx,Yu = fBuildSystem_Linear(M,C,K,Ya,Yv,Yq,Fp=Fp,Pp=Pp,Yp=Yp,Method='augmented_first_order')
 
         # Reference values for test, NOTE: only true because no couplings assumed here
-        Ac_ref, Bc_ref, Gc_ref, Jc_ref = EmptyStateMat(nDOF_ext,nU,nY)
-        Gc_ref[0,0] = -K_twr/M_twr
-        Gc_ref[0,2] = -C_twr/M_twr
-        Gc_ref[0,4] =  1/M_twr
-        Gc_ref[1,3] =  1           # psi_dot = x~[3]
-        Gc_ref[2,6] =  1           # Mq      = x~[6]
+        Xx_ref, Xu_ref, Yx_ref, Yu_ref = EmptyStateMat(nDOF_ext,nU,nY)
+        Yx_ref[0,0] = -K_twr/M_twr
+        Yx_ref[0,2] = -C_twr/M_twr
+        Yx_ref[0,4] =  1/M_twr
+        Yx_ref[1,3] =  1           # psi_dot = x~[3]
+        Yx_ref[2,6] =  1           # Mq      = x~[6]
 
-        np.testing.assert_almost_equal(Gc,Gc_ref)
+        np.testing.assert_almost_equal(Yx,Yx_ref)
 
-#         print('Ac\n',Ac)
-#         print('Bc\n',Bc)
-#         print('Gc\n',Gc)
-#         print('Jc\n',Jc)
+#         print('Xx\n',Xx)
+#         print('Xu\n',Xu)
+#         print('Yx\n',Yx)
+#         print('Yu\n',Yu)
 
 if __name__=='__main__':
     unittest.main()
